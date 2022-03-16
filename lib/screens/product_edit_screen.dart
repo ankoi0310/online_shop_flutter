e5b8ee8806d21b76a1ecca021c1cba59ef2d2434
An <huynhvahuuan3620@gmail.om>
2022-03-17 02:25:53 +0700

    update project

--- lib/screens/product_edit_screen.dart
+++ lib/screens/product_edit_screen.dart
@@ -80,13 +80,13 @@ class _ProductEditScreen extends State<ProductEditScreen> {
         } catch (error) {
           await showDialog(context: context, builder: (ctx) => AlertDialog(
               title: const Text('Error'), content: Text(error.toString(),),
-              actions: <Widget>[FlatButton(onPressed: () => {Navigator.of(ctx).pop(), setState(() {_isLoading = false;}),}, child: Text('Okay'))]
+              actions: <Widget>[TextButton(onPressed: () => {Navigator.of(ctx).pop(), setState(() {_isLoading = false;}),}, child: const Text('Okay'))]
           ));
         }
       } catch (error) {
         await showDialog(context: context, builder: (ctx) => AlertDialog(
             title: const Text('Error'), content: Text(error.toString(),),
-            actions: <Widget>[FlatButton(onPressed: () => {Navigator.of(ctx).pop(), setState(() {_isLoading = false;}),}, child: Text('Okay'))]
+            actions: <Widget>[TextButton(onPressed: () => {Navigator.of(ctx).pop(), setState(() {_isLoading = false;}),}, child: const Text('Okay'))]
         ));
       }
     }
@@ -96,7 +96,7 @@ class _ProductEditScreen extends State<ProductEditScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Edit Product'),
+        title: const Text('Chỉnh sửa sản phẩm'),
         actions: <Widget>[
           IconButton(icon: const Icon(Icons.save), onPressed: _saveForm)
         ],
@@ -105,64 +105,58 @@ class _ProductEditScreen extends State<ProductEditScreen> {
         padding: const EdgeInsets.all(16.0),
         child: Form(key: _form, child: SingleChildScrollView(child: Column(
           children: <Widget>[
-            TextFormField(decoration: const InputDecoration(labelText: 'Title'), textInputAction: TextInputAction.next,
+            TextFormField(decoration: const InputDecoration(labelText: 'Tựa đề'), textInputAction: TextInputAction.next,
               initialValue: _initValues['name'],
               onFieldSubmitted: (_) => FocusScope.of(context).requestFocus(_priceFocusNode),
               onSaved: (value) => _editedProduct = Product(id: _editedProduct.id, name: value!, description: _editedProduct.description, unitPrice: _editedProduct.unitPrice, imageUrl: _editedProduct.imageUrl,),
               validator: (value) {
                 if (value!.isEmpty) {
-                  return 'Please provide a value.';
+                  return 'Hãy nhập dữ liệu.';
                 }
                 return null;
               },
             ),
-            TextFormField(decoration: const InputDecoration(labelText: 'Price'), focusNode: _priceFocusNode, textInputAction: TextInputAction.next, keyboardType: const TextInputType.numberWithOptions(signed: true, decimal: false),
+            TextFormField(decoration: const InputDecoration(labelText: 'Giá tiền'), focusNode: _priceFocusNode, textInputAction: TextInputAction.next, keyboardType: const TextInputType.numberWithOptions(signed: true, decimal: false),
               initialValue: _initValues['unitPrice'],
               onFieldSubmitted: (_) => FocusScope.of(context).requestFocus(_descriptionFocusNode),
               onSaved: (value) => _editedProduct = Product(id: _editedProduct.id, name: _editedProduct.name, description: _editedProduct.description, unitPrice: int.parse(value!), imageUrl: _editedProduct.imageUrl,),
               validator: (value) {
                 if (value!.isEmpty) {
-                  return 'Please enter a price.';
+                  return 'Hãy nhập giá.';
                 }
-                if (double.tryParse(value) == null) {
-                  return 'Please enter a valid number.';
+                if (int.tryParse(value) == null) {
+                  return 'Hãy nhập giá hợp lệ.';
                 }
-                if (double.parse(value) <= 0) {
-                  return 'Please enter a number greater than zero.';
+                if (int.parse(value) <= 0) {
+                  return 'Hãy nhập số lớn hơn 0.';
                 }
                 return null;
               },
             ),
-            TextFormField(decoration: InputDecoration(labelText: 'Description'), focusNode: _descriptionFocusNode, keyboardType: TextInputType.multiline, maxLines: 4,
+            TextFormField(decoration: const InputDecoration(labelText: 'Mô tả'), focusNode: _descriptionFocusNode, keyboardType: TextInputType.multiline, maxLines: 4,
               textInputAction: TextInputAction.next, onFieldSubmitted: (_) => FocusScope.of(context).requestFocus(_imageFocusNode),
               initialValue: _initValues['description'],
               onSaved: (value) => _editedProduct = Product(id: _editedProduct.id, name: _editedProduct.name, description: value!, unitPrice: _editedProduct.unitPrice, imageUrl: _editedProduct.imageUrl,),
             ),
             Row(
               children: [
-                Container(
-                  width: 100,
-                  height: 100,
-                  margin: const EdgeInsets.only(top: 8, left: 10),
-                  decoration: BoxDecoration(
-                    border: Border.all(width: 1, color: Colors.grey),
-                  ),
-                  child: _imageUrlController.text.isEmpty ? Text('Enter an URL') : FittedBox(child: Image.network(_imageUrlController.text), fit: BoxFit.cover,),
+                Container(width: 100, height: 100, margin: const EdgeInsets.only(top: 8, left: 5),
+                  child: _imageUrlController.text.isEmpty ? const Text('Nhập đường dẫn hình ảnh') : FittedBox(child: Image.network(_imageUrlController.text), fit: BoxFit.cover,),
                 ),
                 Expanded(
-                  child: TextFormField(decoration: InputDecoration(labelText: 'Image URL'), focusNode: _imageFocusNode,
+                  child: TextFormField(decoration: const InputDecoration(labelText: 'Đường dẫn hình ảnh'), focusNode: _imageFocusNode,
                     controller: _imageUrlController, keyboardType: TextInputType.url, textInputAction: TextInputAction.done,
                     onEditingComplete: () => setState(() {}),
                     onSaved: (value) => _editedProduct = Product(id: _editedProduct.id, name: _editedProduct.name, description: _editedProduct.description, unitPrice: _editedProduct.unitPrice, imageUrl: value!,),
                     validator: (value) {
                       if (value!.isEmpty) {
-                        return 'Please enter an image URL.';
+                        return 'Hãy nhập đường dẫn hình ảnh.';
                       }
                       if (!value.startsWith('http') && !value.startsWith('https')) {
-                        return 'Please enter a valid URL.';
+                        return 'Vui lòng nhập đường dẫn hợp lệ.';
                       }
                       if (!value.endsWith('.png') && !value.endsWith('.jpg') && !value.endsWith('.jpeg')) {
-                        return 'Please enter a valid image URL.';
+                        return 'Vui lòng nhập ảnh hợp lệ.';
                       }
                       return null;
                     },
@@ -176,6 +170,7 @@ class _ProductEditScreen extends State<ProductEditScreen> {
     );
   }
 
+  @override
   void dispose() {
     _imageFocusNode.removeListener(_updateImageUrl);
     _priceFocusNode.dispose();
